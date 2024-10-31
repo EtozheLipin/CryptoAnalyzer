399cf4225818acafea3da294a477898f06cae20c
Vasiliy Lipin <VasiliyLipin127@yandex.ru>
2024-11-01 02:40:51 +0300

    create Encoder.encrypt

--- src/Constants.java
+++ src/Constants.java
@@ -5,7 +5,9 @@ public class Constants {
             'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
             'ъ', 'ы', 'ь', 'э', 'ю', 'я', '.', ',', '«', '»', '"', '-', '\'', ':', ';', '!', '?', ' '};
 
-    public static final char[] REVERSE_ALPHABET = {'f'};
+    public static final char[] REVERSE_ALPHABET = {' ', '?', '!', ';', ':', '\'', '-', '"', '»', '«', ',', '.', 'я', 'ю',
+            'э', 'ь', 'ы', 'ъ', 'щ', 'ш', 'ч', 'ц', 'х', 'ф', 'у', 'т', 'с', 'р', 'п', 'о', 'н', 'м', 'л', 'к', 'и',
+            'з', 'ж', 'ё', 'е', 'д', 'г', 'в', 'б', 'а'};
 
     public static final Path output = Path.of("C:\\Users\\user\\Desktop\\Мои Проги\\com.javarush.lipin.cryptoanalyzer\\src\\Result.txt");
 }
