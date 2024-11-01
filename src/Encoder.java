91edcf21b481bead9a1a01e951fa6507a35a6358
Vasiliy Lipin <VasiliyLipin127@yandex.ru>
2024-11-01 03:03:20 +0300

    complete Encoder and Decryptor

--- src/Encoder.java
+++ src/Encoder.java
@@ -20,8 +20,8 @@ public class Encoder {
                 line = line.toLowerCase();
                 array = line.toCharArray();
 
-                if (key % 44 == 0) {
-                    System.out.println("Ключ не может быть кратным 44");
+                if (key % 45 == 0) {
+                    System.out.println("Ключ не может быть кратным 45");
                     return;
                 }
                 while (key > Constants.ALPHABET.length) {
@@ -58,7 +58,52 @@ public class Encoder {
     }
 
     public void reversEncrypt() {
+        key = -key;
+        try (BufferedReader br = new BufferedReader(new FileReader(path));
+             BufferedWriter bw = new BufferedWriter(new FileWriter(String.valueOf(Constants.output)))) {
+            char[] array;
+            int length = Constants.ALPHABET.length - 1;
+            String line;
 
+            while ((line = br.readLine()) != null) {
+                line = line.toLowerCase();
+                array = line.toCharArray();
+
+                if (key % 45 == 0) {
+                    System.out.println("Ключ не может быть кратным 45");
+                    return;
+                }
+                while (key > Constants.ALPHABET.length) {
+                    key = key - Constants.ALPHABET.length;
+                }
+
+                for (int i = 0; i < line.length(); i++) {
+                    for (int j = 0; j < Constants.ALPHABET.length; j++) {
+                        if (array[i] == Constants.REVERSE_ALPHABET[j]) {
+                            if (j + key > length) {
+                                int temp = length - j;
+                                if (temp > key) {
+                                    array[i] = Constants.REVERSE_ALPHABET[(key + temp) - 1];
+                                    break;
+                                } else {
+                                    array[i] = Constants.REVERSE_ALPHABET[(key - temp) - 1];
+                                    break;
+                                }
+                            } else {
+                                array[i] = Constants.REVERSE_ALPHABET[(j + key)];
+                                break;
+                            }
+                        }
+                    }
+                }
+                bw.write(array);
+                bw.write("\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Во время шифрования что-то пошло не так!");
+            throw new RuntimeException(e);
+        }
+        System.out.println("Шифрование завершено");
     }
 
 }
